173fe26929437a56df9ec146ad9c2cc8b01e4985
Alexey Solovyov <plotinka@lyoha.info>
2021-07-21 15:38:06 +0300

    fix: broken ssr for markdown pages

--- src/Pages/Markdown.svelte
+++ src/Pages/Markdown.svelte
@@ -1,20 +1,26 @@
 <script>
     import useCurrentRoute from '@easyroute/svelte/useCurrentRoute'
     import MarkdownIt from 'markdown-it'
+    import { isBrowser } from '@easyroute/core/utils/isBrowser'
 
     export let router;
 
     const currentRoute = useCurrentRoute()
     const md = new MarkdownIt()
+    const SSR = !isBrowser()
     let content = ''
     let timerDelay = 0
 
     function renderText(meta) {
         if (!meta.pageText) return
-        setTimeout(() => {
+        if (SSR) {
             content = md.render(meta.pageText)
-            timerDelay = 200
-        }, timerDelay)
+        } else {
+            setTimeout(() => {
+                content = md.render(meta.pageText)
+                timerDelay = 200
+            }, timerDelay)
+        }
     }
 
     $: renderText($currentRoute.meta)
